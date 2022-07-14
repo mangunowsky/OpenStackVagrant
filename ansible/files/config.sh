eaf92f94aa1566eebfa48e6450635f53ac9bafdb
Michal <michal.magun@icloud.com>
2022-07-14 23:46:34 +0200

    Cleanup

--- ansible/files/config.sh
+++ ansible/files/config.sh
@@ -4,8 +4,6 @@ DEVSTACK_IP=$(ip -4 addr show $1 | grep -P inet | awk '{print substr($2, 1, leng
 
 DEVSTACK_IP=$(echo $DEVSTACK_IP | xargs)
 
-echo $DEVSTACK_IP
-
 cp /opt/stack/devstack/stackrc /opt/stack/devstack/stackrc.bak
 
 sed -i "s/HOST_IP=\${HOST_IP:-}/HOST_IP=\${HOST_IP:-$DEVSTACK_IP}/g" /opt/stack/devstack/stackrc
